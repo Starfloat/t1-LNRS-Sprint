6d9d98d5d0d5ae3e43a38fe20a454f2bcef15eb2
Sunni Lakhaisy <lakhaisys1@southernct.edu>
2022-05-06 16:36:44 -0400

    variable fixes

--- terraform-manifests/resource-group.tf
+++ terraform-manifests/resource-group.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.storage_account_name}-${random_string.myrandom.id}"
+  name                     = "${var.storage_account_name}${random_string.myrandom.id}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
--- terraform-manifests/sql-database.tf
+++ terraform-manifests/sql-database.tf
@@ -1,23 +1,23 @@
 
 resource "azurerm_mysql_server" "mysql_server" {
-  name                         = "${local.resource_name_prefix}-${var.mysql_db_name}-server-${random_string.myrandom.id}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  
+  name                = "${local.resource_name_prefix}-${var.mysql_db_name}-server-${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
   administrator_login          = var.mysql_db_username
   administrator_login_password = var.mysql_db_password
 
-  version  = "8.0"
-  sku_name = "GP_Gen5_2"
+  version    = "8.0"
+  sku_name   = "GP_Gen5_2"
   storage_mb = 5120
 
-  auto_grow_enabled = true
-  backup_retention_days = 7
-  geo_redundant_backup_enabled = false
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
-  public_network_access_enabled = true
-  ssl_enforcement_enabled = false
-  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = false
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
 
   tags = local.common_tags
 }
@@ -26,22 +26,22 @@ resource "azurerm_mysql_database" "mysql_database" {
   name                = var.mysql_db_schema
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_server.mysql_server.name
-  charset = "utf8"
-  collation = "utf8_unicode_ci"
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 
 }
 
 resource "azurerm_mysql_firewall_rule" "mysql_fw_rule" {
-  name = "allow-access-from-linuxvm-publicip"
+  name                = "allow-access-from-linuxvm-publicip"
   resource_group_name = azurerm_resource_group.rg.name
-  server_name = azurerm_mysql_server.mysql_server.name
-  start_ip_address = azurerm_public_ip.wordpress_linuxvm_publicip.ip_address
-  end_ip_address = azurerm_public_ip.wordpress_linuxvm_publicip.ip_address
+  server_name         = azurerm_mysql_server.mysql_server.name
+  start_ip_address    = azurerm_public_ip.wordpress_linuxvm_publicip.ip_address
+  end_ip_address      = azurerm_public_ip.wordpress_linuxvm_publicip.ip_address
 }
 
 resource "azurerm_mysql_virtual_network_rule" "azurerm_mysql_virtual_network_rule" {
-  name = "mysql-vnet-rule"
+  name                = "mysql-vnet-rule"
   resource_group_name = azurerm_resource_group.rg.name
-  server_name = azurerm_mysql_server.mysql_server.name
-  subnet_id = azurerm_subnet.wordpress_subnet.id
+  server_name         = azurerm_mysql_server.mysql_server.name
+  subnet_id           = azurerm_subnet.wordpress_subnet.id
 }
